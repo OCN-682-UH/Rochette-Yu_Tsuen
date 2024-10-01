b9c688eba418886d2880bd9f689a3d3c81f7c356
keanuryt <keanuryt@hawaii.edu>
2024-10-01 11:16:28 -1000

    Updating HW assignment based on feedbacks

--- Week_05/scripts/Week_05_HW.R
+++ Week_05/scripts/Week_05_HW.R
@@ -20,28 +20,28 @@ cond_data <- cond_data %>% mutate(date = mdy_hms(date)) %>%
 joined_df <- inner_join(cond_data, depth_data)
 
 # Calculating the average of each variable by minute and graphing it
-joined_df %>% mutate(minutes = minute(date)) %>% 
-              #create a new column by extracting minutes in the date time
+joined_df %>% mutate(date = round_date(date, "minute")) %>% 
+              #round the data to the nearest minute 
   rename(Absolute_Pressure = AbsPressure) %>% 
               # I didn't like the name so I made it more explicit 
   pivot_longer(cols = c(2:5, -3), names_to = "Parameters", 
                values_to = "Values") %>% 
               # Turn the data in long format for summary 
-  group_by(minutes, Parameters) %>% # group by minutes and parameters
+  group_by(date, Parameters) %>% # group by minutes and parameters
   summarize(mean_val = mean(Values), .groups = NULL) %>% 
               # calculating the average and removing grouping 
-  ggplot(aes(x= minutes, y = mean_val, color = Parameters)) +
+  ggplot(aes(x= date, y = mean_val, color = Parameters)) +
               # setting the mapping of the graph
   geom_point() + # scatter plot
   geom_line() + # with a line that connects the dots
   facet_wrap(~ Parameters, ncol = 1, scales = "free") +
               # Display the graphs in one column by parameter
               # Each of the graph has its only y-axis units
-  labs(title = "Average Physic-Chemical Values by Minute",
+  labs(title = "Average Physico-Chemical Values by Minute",
        subtitle = "Taken in Moorea, FP",
-       x = "Minute",
+       x = "Date-Time",
        y = "Average Values (units are different)")+ #give explicit labels
-  theme_classic() +
+  theme_bw() +
   theme(plot.title = element_text(size=20,color = "#FD8A8A"), 
         #edit the title of the plot
         plot.subtitle = element_text(size=14,color = "#9EA1D4"),
@@ -53,8 +53,6 @@ joined_df %>% mutate(minutes = minute(date)) %>%
         panel.background = element_rect(fill = "azure"),
         legend.position = "none") +
         # removing the legend, unecessary here in my opinion
-  scale_x_continuous(breaks = seq(0,60,5))+
-  # set the x-axis to display minutes every 5 units. 
   scale_color_ghibli_d("SpiritedMedium", direction = -1)
   # decide on cool ghibli inspired colors for the graph
 
